311bd5ef33318d79534b0efdad34f24fe83ade82
Kevin Hagens <kevin.hagens@CHIBRTPML010001.local>
2018-10-15 09:07:05 -0500

    updates

---
+++ services/slack.js
@@ -0,0 +1,12 @@
+const debug = require('debug')('Sailor:Slack');
+const config = require('./../config');
+const IncomingWebhook = require('@slack/client').IncomingWebhook;
+
+class Slack{
+
+	constructor(){
+
+	}
+}
+
+module.exports = new Slack();
