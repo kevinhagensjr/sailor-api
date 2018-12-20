740a030ee66dc13d0aaacf9b428d72fa867f73ce
kevinhagensjr <khagens92@gmail.com>
2018-12-20 17:35:02 -0600

    updates craigslist

--- controllers/v1/sale.js
+++ controllers/v1/sale.js
@@ -174,7 +174,9 @@ class SaleController{
 
 		const address = await this.userModel.getAddress(userID);
 		if(address.zipcode){
-			let craigslistClient = new craigslist.Client();
+			let craigslistClient = new craigslist.Client({
+				city : 'stlouis'
+			});
 			let listings = await craigslistClient.search({
 				category : 'gms', //garage sale category
 				postal : 63033,
